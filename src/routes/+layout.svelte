a3cb2269c55c786b9b41cdbc0728674b54ec8368
Anh Nguyen <anguyen@anhs-mbp.kbtm.kangan.edu.au>
2023-07-18 12:54:22 +1000

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<nav>
+<ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="/Text_Tutorials">Text Tutorials</a></li>
+</ul>
+</nav>
+
+<div>
+    <slot />
+</div>
+
+<style>
+    nav {
+  background-color: #333;
+  color: #fff;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline-block;
+}
+
+nav ul li a {
+  display: block;
+  padding: 10px 20px;
+  color: #fff;
+  text-decoration: none;
+}
+
+nav ul li a:hover {
+  background-color: #555;
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div> 
+    <h4>This site contains various links to Svelte things</h4>
+</div>
