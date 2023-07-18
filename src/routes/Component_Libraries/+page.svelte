8c8782fcce52743287cb81bbf5d95a8c4a067f7c
djhyneskangan <200020659@student.kangan.edu.au>
2023-07-18 14:25:47 +1000

    links and page added

---
+++ src/routes/Component_Libraries/+page.svelte
@@ -0,0 +1,28 @@
+<div>
+    <ul class="list">
+        <li><a href="https://dev.to/plazarev/overview-of-svelte-ui-libraries-and-components-2ban">Overview of Svelte UI Libraries and Components</a></li>
+        <li><a href="https://www.youtube.com/watch?v=teXFHcugXaI">Create Your Own Svelte Component Library</a></li>
+        <li><a href="https://bestofsvelte.com/t/ui-library">SvelteKit blog demo</a>
+        </ul>
+</div>
+
+
+<style>
+    .list {
+        list-style: none;
+    }
+
+    .list a {
+        color: rgb(21, 7, 7);
+        text-decoration: none;
+    }
+
+    div
+    {
+        padding: 10px;
+        border: 2px solid rgb(21, 7, 7);
+        border-radius: 5px;
+        margin: 10px auto;
+    }
+
+</style>
